25c323489acfec5656f2d39e2cbd4b9e193ea1a5
Fatima Garcia <garzifati09@gmail.com>
2024-12-17 00:15:47 -0600

    Change to receive end proccess chunks in multer

--- src/controllers/fileController.js
+++ src/controllers/fileController.js
@@ -1,101 +1,148 @@
 const combineChunks = require("../helpers/combineChunks");
 const File = require("../models/fileModel");
-const path = require('path');
-const fs = require('fs').promises;
+const path = require("path");
+const fs = require("fs").promises;
 
 exports.uploadFile = async (req, res) => {
-  console.log(req.file)
+  console.log("file", req.file);
   if (!req.file) {
     return res.status(400).json({ message: "No se ha cargado ningún archivo" });
   }
 
   const { originalName, fileName, totalChunks, chunkIndex } = req.body;
+  console.log("originalName", originalName);
 
   try {
+    // Buscar o crear registro en la base de datos
     let fileRecord = await File.findOne({ fileName });
-
+    console.log("fileRecord", fileRecord);
     if (!fileRecord) {
-
       fileRecord = new File({
         originalName,
         fileName,
         totalChunks,
         uploadedChunks: 0,
-        fileSize: 0,
+        fileSize: 0, // Se actualizará al final
         fileType: req.file.mimetype,
         isComplete: false,
       });
-
       await fileRecord.save();
     }
 
-    // Guardamos el chunk en la carpeta de uploads
-    const chunkPath = path.join(__dirname, 'uploads', `${fileName}_chunk_${chunkIndex}`);
-    await fs.promises.writeFile(chunkPath, req.file.buffer);
+    // Guardar el chunk en la carpeta temporal
+    const tempDir = path.join("uploads", "temp");
+    console.log("tempDir", tempDir);
 
-    // Actualizamos el número de chunks subidos en la base de datos
-    fileRecord.uploadedChunks += 1;
-
-    if (fileRecord.uploadedChunks === totalChunks) {
-      fileRecord.isComplete = true;
-
-      // Combinamos los chunks en un archivo final
-      const finalFilePath = path.join(__dirname, 'uploads', fileName);
-      await combineChunks(fileName, totalChunks, finalFilePath);
-
-      fileRecord.filePath = finalFilePath;
-      fileRecord.fileSize = fs.statSync(finalFilePath).size;
+    // Verificar si la carpeta 'temp' existe, si no, crearla
+    try {
+      await fs.access(tempDir); // Verifica si existe
+    } catch (error) {
+      await fs.mkdir(tempDir, { recursive: true }); // Crea la carpeta si no existe
+      console.log("Carpeta temporal creada.");
     }
 
-    await fileRecord.save();
+    const chunkPath = path.join(tempDir, `${fileName}_chunk_${chunkIndex}`);
+    console.log(" req.file.buffer", req.file.buffer);
+    if (!req.file || !req.file.buffer) {
+      return res.status(400).json({ message: 'El archivo está vacío o no se ha recibido correctamente.' });
+    }
+    await fs.writeFile(chunkPath, req.file.buffer); 
 
-    res.status(200).json({ message: "Chunk subido exitosamente", uploadedChunks: fileRecord.uploadedChunks });
+    // Actualizar progreso en la base de datos
+    try{
+      fileRecord.uploadedChunks += 1;
+      await fileRecord.save();
 
+    } catch(err) {
+      console.log(err)
+      return;
+    }
 
-  } catch (error) {
+    console.log(`Chunk ${chunkIndex} subido correctamente.`);
+
+    // Ensamblar archivo si todos los chunks están completos
+    console.log('mm', fileRecord.uploadedChunks === parseInt(totalChunks))
+    if (fileRecord.uploadedChunks === parseInt(totalChunks)) {
+      const finalFilePath = path.join("uploads", fileName);
+      console.log(finalFilePath)
+      const writeStream = fs.createWriteStream(finalFilePath);
+
+      for (let i = 0; i < totalChunks; i++) {
+        const currentChunkPath = path.join(tempDir, `${fileName}_chunk_${i}`);
+        const chunk = fs.readFileSync(currentChunkPath);
+        writeStream.write(chunk);
+        fs.unlinkSync(currentChunkPath); // Eliminar chunk después de ensamblarlo
+      }
+
+      writeStream.end();
+
+      // Actualizar estado en la base de datos
+      fileRecord.isComplete = true;
+      fileRecord.fileSize = fs.statSync(finalFilePath).size;
+      console.log('fileRecord.fileSize', fileRecord.fileSize)
+      try{
+        await fileRecord.save();
+
+      } catch(err){
+        console.log(err);
+        return;
+      }
+
+      return res
+        .status(200)
+        .json({
+          message: "Archivo ensamblado con éxito.",
+          filePath: finalFilePath,
+        });
+    }
+
     res
-      .status(500)
-      .json({
-        message: "Error al guardar el archivo en la base de datos",
-        error,
-      });
+      .status(200)
+      .json({ message: `Chunk ${chunkIndex} subido correctamente.` });
+  } catch (error) {
+    res.status(500).json({
+      message: "Error al guardar el archivo en la base de datos",
+      error,
+    });
   }
 };
 
 exports.getFile = async (req, res) => {
   try {
     const file = await File.findById(req.params.id);
-    console.log(file)
+    console.log(file);
     if (!file) {
-      return res.status(404).json({ message: 'Archivo no encontrado' });
+      return res.status(404).json({ message: "Archivo no encontrado" });
     }
 
     res.status(200).json(file);
   } catch (error) {
-    res.status(500).json({ message: 'Error al obtener el archivo', error });
+    res.status(500).json({ message: "Error al obtener el archivo", error });
   }
-}
+};
 
 exports.downloadFile = async (req, res) => {
   try {
     const file = await File.findById(req.params.id);
     if (!file) {
-      return res.status(404).json({ message: 'Archivo no encontrado' });
+      return res.status(404).json({ message: "Archivo no encontrado" });
     }
 
     // Enviar el archivo como descarga
     res.download(file.filePath, file.originalName, (err) => {
       if (err) {
-        console.log('Error al enviar el archivo')
+        console.log("Error al enviar el archivo");
         if (!res.headersSent) {
-          return res.status(500).json({ message: 'Error al mostrar el archivo', error: err });
+          return res
+            .status(500)
+            .json({ message: "Error al mostrar el archivo", error: err });
         }
       }
-    })
+    });
   } catch (error) {
-    res.status(500).json({ message: 'Error al obtener el archivo', error });
+    res.status(500).json({ message: "Error al obtener el archivo", error });
   }
-}
+};
 
 // Función para obtener todos los archivos
 exports.getAllFiles = async (req, res) => {
@@ -105,24 +152,23 @@ exports.getAllFiles = async (req, res) => {
 
     // Si no hay archivos, devolver un mensaje
     if (files.length === 0) {
-      return res.status(404).json({ message: 'No se encontraron archivos' });
+      return res.status(404).json({ message: "No se encontraron archivos" });
     }
 
     // Devolver los archivos en formato JSON
     res.status(200).json({ files });
   } catch (error) {
     // Manejo de errores si la operación de búsqueda falla
-    res.status(500).json({ message: 'Error al obtener los archivos', error });
+    res.status(500).json({ message: "Error al obtener los archivos", error });
   }
 };
-
 
 exports.deleteFile = async (req, res) => {
   try {
     const file = await File.findById(req.params.id);
-    console.log('fileDelete', file)
+    console.log("fileDelete", file);
     if (!file) {
-      return res.status(404).json({ message: 'Archivo no encontrado' });
+      return res.status(404).json({ message: "Archivo no encontrado" });
     }
     const filePath = path.resolve(file.filePath);
 
@@ -132,11 +178,12 @@ exports.deleteFile = async (req, res) => {
     // Eliminar el registro de la base de datos
     const deletedFile = await File.findByIdAndDelete(req.params.id);
     if (!deletedFile) {
-      return res.status(500).json({ message: 'Error al eliminar el archivo de la base de datos' });
+      return res
+        .status(500)
+        .json({ message: "Error al eliminar el archivo de la base de datos" });
     }
-    res.status(200).json({ message: 'Archivo eliminado con éxito' });
-
+    res.status(200).json({ message: "Archivo eliminado con éxito" });
   } catch (error) {
-    res.status(500).json({ message: 'Error al eliminar el archivo', error });
+    res.status(500).json({ message: "Error al eliminar el archivo", error });
   }
-}
+};
--- src/middlewares/uploadMiddleware.js
+++ src/middlewares/uploadMiddleware.js
@@ -1,29 +1,22 @@
 const multer = require('multer');
-const fs = require('fs');
+const fs = require('fs').promises;  
 const path = require('path');
 
-// Configuración de Multer para el almacenamiento de archivos
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    console.log('first')
-    // Valida que la carpeta de destino exista
-    const dir = 'uploads';
-    if (!fs.existsSync(dir)) {
-      fs.mkdirSync(dir);
-    }
-    cb(null, dir); // Carpeta 'uploads' donde se guardarán los archivos
-  },
-  filename: (req, file, cb) => {
-    console.log(req)
-    const { fileName, chunkIndex } = req.body;
-    cb(null, `${fileName}_chunk_${chunkIndex}${path.extname(file.originalname)}`); // Nombre único para cada archivo
+const ensureDirExists = async (dir) => {
+  try {
+    await fs.access(dir); // Verifica si la carpeta existe
+  } catch {
+    await fs.mkdir(dir, { recursive: true }); // Crea la carpeta si no existe
   }
-});
+};
+
+// Configuración de Multer para el almacenamiento de archivos
+const storage = multer.memoryStorage();
 
 // Configuración de los límites de carga, en este caso, 3GB máximo
 const upload = multer({
   storage: storage,
-  limits: { fileSize: 3 * 1024 * 1024 * 1024 } // 3GB en bytes
-}).none();
+  limits: { fileSize: 5 * 1024 * 1024 * 1024 } // 5GB en bytes
+}).single('chunk');
 
 module.exports = upload;
